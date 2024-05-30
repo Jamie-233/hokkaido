346edf1f442498cd29c3e0fab9cde5c22a32b563
Jenkin <jiancunge.com@gmail.com>
2024-05-31 00:19:04 +0900

    feat: beautiful layout

---
+++ src/config/menu.ts
@@ -0,0 +1,56 @@
+export const sidebarMenuList = [
+    {
+        path: '/dashboard',
+        redirect: '/dashboard/home',
+        name: 'Dashboard',
+        meta: {},
+        children: []
+    },
+    {
+        path: '/dashboard',
+        redirect: '/dashboard/home',
+        name: '商品设定',
+        meta: {},
+        children: [
+            {
+                path: '/dashboard',
+                redirect: '/dashboard/home',
+                name: '商品管理',
+                meta: {}
+            },
+            {
+                path: '/dashboard',
+                redirect: '/dashboard/home',
+                name: '销售额',
+                meta: {}
+            },
+            {
+                path: '/dashboard',
+                redirect: '/dashboard/home',
+                name: '销售额2',
+                meta: {}
+            }
+        ]
+    },
+    {
+        path: '/dashboard',
+        redirect: '/dashboard/manage',
+        name: 'master管理',
+        meta: {},
+        children: []
+    },
+    {
+        path: '/dashboard',
+        redirect: '/dashboard/info',
+        name: 'Info',
+        meta: {},
+        children: []
+    },
+    {
+        path: '/dashboard',
+        redirect: '/dashboard/pay',
+        name: 'Pay',
+        meta: {},
+        children: []
+    }
+];
---
+++ src/layout/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Sidebar from './sidebar.vue';
+// import { ref } from "vue";
+
+// defineProps<{ msg: string }>();
+
+// const count = ref(0);
+</script>
+
+<template>
+  <Sidebar />
+</template>
+
+<style scoped>
+</style>
