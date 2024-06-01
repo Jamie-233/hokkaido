e1a272510ec7fa832bf1499e1f356561a8369a95
Jenkin <jiancunge.com@gmail.com>
2024-06-01 15:49:52 +0900

    chore: beautify dev and build config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import ElementPlus from 'unplugin-element-plus/vite';
+import { join } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import ElementPlus from 'unplugin-element-plus/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), ElementPlus()]
-});
+  plugins: [vue(), ElementPlus()],
+  resolve: {
+    alias: {
+      '@': join(__dirname, '/src')
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: '',
+        changeOrigin: true
+      }
+    }
+  }
+})
